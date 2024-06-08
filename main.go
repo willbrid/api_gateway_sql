7e4b85b071ba84e814f380845d629681336d1f40
willbrid <ngaswilly77@gmail.com>
2024-06-08 23:17:53 +0100

    feat: init server with handler

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"db-api-sql/apisql"
+	"db-api-sql/config"
 	"db-api-sql/logging"
 
 	"flag"
+	"net/http"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 )
 
 var validate *validator.Validate
@@ -27,7 +32,22 @@ func main() {
 		return
 	}
 
-	// Load config file
-
+	configLoaded, err := config.LoadConfig(configFile, validate)
+	if err != nil {
+		logging.Log(logging.Error, "error loading configuration")
+		return
+	}
 	logging.Log(logging.Info, "configuration file '%s' was loaded successfully", configFile)
+
+	apisqlInstance := apisql.NewApiSql(configLoaded)
+	router := mux.NewRouter()
+	router.HandleFunc("/dbapisql/{targetname}", apisqlInstance.ApiSqlHandler).Methods("POST")
+
+	strListenPort := strconv.Itoa(listenPort)
+	logging.Log(logging.Info, "server is listening on port %v", strListenPort)
+	err = http.ListenAndServe(":"+strListenPort, router)
+	if err != nil {
+		logging.Log(logging.Error, "failed to start server: %v", err.Error())
+		return
+	}
 }
