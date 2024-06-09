ba01a8ff68b8768c9e9a26e7e691efbae2900ec8
willbrid <ngaswilly77@gmail.com>
2024-06-09 10:44:41 +0100

    feat: add databases field test

--- config/config_test.go
+++ config/config_test.go
@@ -133,3 +133,215 @@ db_api_sql:
 		t.Errorf("no error message found")
 	}
 }
+
+func TestDabatasesField(t *testing.T) {
+	configSlices := []string{
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: ""
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: ""
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "xxxxx"
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: ""
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0"
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0.0.1"
+    port: ""
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0.0.1"
+    port: "1000"
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0.0.1"
+    port: "49152"
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0.0.1"
+    port: "3306"
+    username: ""
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0.0.1"
+    port: "3306"
+    username: "xxxxx"
+    password: ""
+`,
+		`---
+db_api_sql:
+  timeout: "10s"
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxxx
+  databases:
+  - name: "xxxxx"
+    type: "mariadb"
+    host: "127.0.0.1"
+    port: "3306"
+    username: "xxxxx"
+    password: "xxxxx"
+    dbname: ""
+`,
+	}
+
+	expectations := []string{
+		"validation failed on field 'Databases' for condition 'gt'",
+		"validation failed on field 'Databases' for condition 'gt'",
+		"validation failed on field 'Name' for condition 'required'",
+		"validation failed on field 'Name' for condition 'max'",
+		"validation failed on field 'Type' for condition 'required'",
+		"validation failed on field 'Type' for condition 'oneof'",
+		"validation failed on field 'Host' for condition 'required'",
+		"validation failed on field 'Host' for condition 'ipv4'",
+		"validation failed on field 'Port' for condition 'required'",
+		"validation failed on field 'Port' for condition 'min'",
+		"validation failed on field 'Port' for condition 'max'",
+		"validation failed on field 'Username' for condition 'required'",
+		"validation failed on field 'Password' for condition 'required'",
+		"validation failed on field 'Dbname' for condition 'required'",
+		"",
+	}
+
+	for index, configContent := range configSlices {
+		t.Run(fmt.Sprintf("LoadConfig #%v", index), func(subT *testing.T) {
+			filename, err := utils.CreateConfigFileForTesting(configContent)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			defer os.Remove(filename)
+
+			_, err = LoadConfig(filename, validate)
+
+			expected := expectations[index]
+
+			if err == nil {
+				t.Errorf("no error returned, expected:\n%v", expected)
+			}
+
+			if err.Error() != expected {
+				t.Errorf("\nexpected:\n%v\ngot:\n%v", expected, err.Error())
+			}
+		})
+	}
+}
