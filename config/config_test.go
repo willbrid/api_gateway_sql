84d6e5fdcd20d39199b5998035559c9973a90a8c
willbrid <ngaswilly77@gmail.com>
2024-06-09 10:11:25 +0100

    feat: add testing auth field

--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"db-api-sql/utils"
+
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
@@ -37,5 +41,72 @@ func TestLoadConfigFileNotExist(t *testing.T) {
 
 	if err.Error() != expected {
 		t.Errorf("\nexpected:\n%v\ngot:\n%v", expected, err.Error())
+	}
+}
+
+func TestAuthFieldWithAuthEnabled(t *testing.T) {
+	configSlices := []string{
+		`---
+db_api_sql:
+  auth:
+    enabled: true
+    username: ""
+`,
+		`---
+db_api_sql:
+  auth:
+    enabled: true
+    username: "x"
+`,
+		`---
+db_api_sql:
+  auth:
+    enabled: true
+    username: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+`,
+		`---
+db_api_sql:
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: ""
+`,
+		`---
+db_api_sql:
+  auth:
+    enabled: true
+    username: "xxxxx"
+    password: xxxxxxx
+`,
+	}
+
+	expectations := []string{
+		"validation failed on field 'Username' for condition 'required_if'",
+		"validation failed on field 'Username' for condition 'min'",
+		"validation failed on field 'Username' for condition 'max'",
+		"validation failed on field 'Password' for condition 'required_if'",
+		"validation failed on field 'Password' for condition 'min'",
+	}
+
+	for index, configContent := range configSlices {
+		t.Run(fmt.Sprintf("LoadConfig #%v", index), func(subT *testing.T) {
+			filename, err := utils.CreateConfigFileForTesting(configContent)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			defer os.Remove(filename)
+
+			_, err = LoadConfig(filename, validate)
+
+			expected := expectations[index]
+
+			if err == nil {
+				t.Errorf("no error returned, expected:\n%v", expected)
+			}
+
+			if err.Error() != expected {
+				t.Errorf("\nexpected:\n%v\ngot:\n%v", expected, err.Error())
+			}
+		})
 	}
 }
