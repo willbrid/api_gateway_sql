f7becb1544e66ec0e2cdb0fbec27db738f8ec4a6
willbrid <ngaswilly77@gmail.com>
2024-06-09 11:46:59 +0100

    update: change handler receiver

--- apisql/handler.go
+++ apisql/handler.go
@@ -2,7 +2,7 @@ package apisql
 
 import "net/http"
 
-func (apisql ApiSql) ApiSqlHandler(resp http.ResponseWriter, req *http.Request) {
+func (apisql *ApiSql) ApiSqlHandler(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusOK)
 }
