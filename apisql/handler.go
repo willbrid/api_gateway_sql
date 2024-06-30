10949937cf3515c388b1ca6eac8d675f3fbd7ce9
willbrid <ngaswilly77@gmail.com>
2024-06-30 16:56:59 +0100

    feat: validate param targetname param

--- apisql/handler.go
+++ apisql/handler.go
@@ -1,8 +1,30 @@
 package apisql
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func (apisql *ApiSql) ApiSqlHandler(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	targetName := vars["targetname"]
+
+	target, exist := apisql.config.GetTargetByName(targetName)
+	if !exist {
+		http.Error(resp, fmt.Sprintf("the specified target name %s does not exist", targetName), http.StatusBadRequest)
+		return
+	}
+
+	database, exist := apisql.config.GetDatabaseByDataSourceName(target.DataSourceName)
+	if !exist {
+		http.Error(resp, fmt.Sprintf("the configured datasource name %s does not exist", target.DataSourceName), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("DBNAME : "+database.Dbname, " TARGET NAME : "+target.Name)
+
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusOK)
 }
