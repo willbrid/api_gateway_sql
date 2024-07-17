a3430654471324973dccec876b6d62f6ba9890cd
willbrid <ngaswilly77@gmail.com>
2024-07-17 23:19:47 +0100

    feat: add http utils

---
+++ utils/httputil/httputil.go
@@ -0,0 +1,31 @@
+package httputil
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type HTTPResp struct {
+	Code    int         `json:"code" example:"200"`
+	Message string      `json:"message" example:"status ok"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func SendJSONResponse(resp http.ResponseWriter, status int, message string, data interface{}) {
+	response := HTTPResp{
+		Code:    status,
+		Message: message,
+		Data:    data,
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(status)
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp.Write(jsonResponse)
+}
