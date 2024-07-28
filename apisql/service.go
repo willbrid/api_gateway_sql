b43d2b953d63171adf808d474765ca944b569147
willbrid <ngaswilly77@gmail.com>
2024-07-28 18:53:00 +0100

    refactor: update getTargetAndDatabase func argument with config struct

--- apisql/service.go
+++ apisql/service.go
@@ -10,13 +10,13 @@ import (
 	"gorm.io/gorm"
 )
 
-func getTargetAndDatabase(apisql *ApiSql, targetName string) (*config.Target, *config.Database, error) {
-	target, exist := apisql.config.GetTargetByName(targetName)
+func getTargetAndDatabase(config config.Config, targetName string) (*config.Target, *config.Database, error) {
+	target, exist := config.GetTargetByName(targetName)
 	if !exist {
 		return nil, nil, fmt.Errorf("the specified target name %s does not exist", targetName)
 	}
 
-	database, exist := apisql.config.GetDatabaseByDataSourceName(target.DataSourceName)
+	database, exist := config.GetDatabaseByDataSourceName(target.DataSourceName)
 	if !exist {
 		return nil, nil, fmt.Errorf("the configured datasource name %s does not exist", target.DataSourceName)
 	}
