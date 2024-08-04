59b246c17a4de1c4bd680992d59b1445cf64376a
willbrid <ngaswilly77@gmail.com>
2024-08-04 18:25:12 +0100

    fix: parsed timeout field to int

--- db/postgres.go
+++ db/postgres.go
@@ -19,7 +19,7 @@ func (postgresDB *PostgresDatabase) Connect(db config.Database) (*gorm.DB, error
 		sslMode = "enable"
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v sslmode=%v connect_timeout=%v", db.Host, db.Username, db.Password, db.Dbname, db.Host, sslMode, db.Timeout)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v sslmode=%v connect_timeout=%v", db.Host, db.Username, db.Password, db.Dbname, db.Host, sslMode, int(db.Timeout))
 
 	cnx, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
