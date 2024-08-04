64d6d3fe1d1dd93f9ee4aebe2056ab815f91bcdc
willbrid <ngaswilly77@gmail.com>
2024-08-04 06:19:16 +0100

    feat: add read buffer block csv file

--- utils/file/file.go
+++ utils/file/file.go
@@ -1,7 +1,11 @@
 package file
 
 import (
+	"api-gateway-sql/logging"
+
+	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -18,4 +22,44 @@ func CreateConfigFileForTesting(configContent string) (string, error) {
 	configFile.Close()
 
 	return configFile.Name(), nil
+}
+
+func ReadCSVInBuffer(filePath string, bufferSize int) ([][][]string, error) {
+	var (
+		buffers [][][]string
+		file    *os.File
+		err     error = nil
+	)
+
+	file, err = os.Open(filePath)
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	for {
+		buffer := make([][]string, bufferSize)
+
+		for i := 0; i < bufferSize; i++ {
+			record, err := reader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+			buffer = append(buffer, record)
+		}
+
+		if len(buffer) == 0 {
+			break
+		}
+
+		buffers = append(buffers, buffer)
+	}
+
+	return buffers, nil
 }
