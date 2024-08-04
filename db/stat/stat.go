900856a40e6a9c51449131a609ba94c46cb70745
willbrid <ngaswilly77@gmail.com>
2024-08-04 18:47:55 +0100

    feat: integrate Block field implementation

--- db/stat/stat.go
+++ db/stat/stat.go
@@ -19,21 +19,31 @@ func Connect(sqlitedb string) (*gorm.DB, error) {
 
 // BatchStatistic used to save statistics of all batch requests in memory
 type BatchStatistic struct {
-	ID            string         `json:"id"`
-	TargetName    string         `json:"target"`
-	SuccessCount  int            `json:"success" gorm:"default:0"`
-	FailureCount  int            `json:"failure" gorm:"default:0"`
-	CreatedAt     time.Time      `json:"created_at"`
-	UpdatedAt     time.Time      `json:"updated_at"`
-	FailureRanges []FailureRange `json:"failure_ranges" gorm:"foreignKey:BatchStatisticID"`
+	ID         string    `json:"id"`
+	TargetName string    `json:"target"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	Blocks     []Block   `json:"blocks" gorm:"foreignKey:BatchStatisticID"`
+}
+
+type Block struct {
+	ID               string         `json:"id"`
+	StartLine        int            `json:"start_line"`
+	EndLine          int            `json:"end_line"`
+	SuccessCount     int            `json:"success" gorm:"default:0"`
+	FailureCount     int            `json:"failure" gorm:"default:0"`
+	CreatedAt        time.Time      `json:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at"`
+	FailureRanges    []FailureRange `json:"failure_ranges" gorm:"foreignKey:BlockID"`
+	BatchStatisticID string
 }
 
 type FailureRange struct {
-	ID               string    `json:"id"`
-	StartLine        int       `json:"start_line"`
-	EndLine          int       `json:"end_line"`
-	CreatedAt        time.Time `json:"created_at"`
-	BatchStatisticID string
+	ID        string    `json:"id"`
+	StartLine int       `json:"start_line"`
+	EndLine   int       `json:"end_line"`
+	CreatedAt time.Time `json:"created_at"`
+	BlockID   string
 }
 
 func NewBatchStatistic(targetName string) *BatchStatistic {
@@ -41,6 +51,24 @@ func NewBatchStatistic(targetName string) *BatchStatistic {
 		ID:         uuid.GenerateUID(),
 		CreatedAt:  time.Now(),
 		TargetName: targetName,
+	}
+}
+
+func NewBlock(startLine, endLine int) *Block {
+	return &Block{
+		ID:        uuid.GenerateUID(),
+		StartLine: startLine,
+		EndLine:   endLine,
+		CreatedAt: time.Now(),
+	}
+}
+
+func NewFailureRange(startLine, endLine int) *FailureRange {
+	return &FailureRange{
+		ID:        uuid.GenerateUID(),
+		StartLine: startLine,
+		EndLine:   endLine,
+		CreatedAt: time.Now(),
 	}
 }
 
@@ -58,7 +86,32 @@ func AddBatchStatistic(sqlitedb string, bs *BatchStatistic) error {
 	return cnx.Create(bs).Error
 }
 
-func UpdateBatchStatistic(sqlitedb string, bs *BatchStatistic, isSuccess bool, failureStartLine int, failureEndLine int) (*BatchStatistic, error) {
+func AddNewBlockToBatchStatistic(sqlitedb string, bs *BatchStatistic, startLine int, endLine int) (*Block, error) {
+	cnx, err := Connect(sqlitedb)
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		return nil, err
+	}
+	defer func() {
+		dbCnx, _ := cnx.DB()
+		dbCnx.Close()
+	}()
+
+	bs.UpdatedAt = time.Now()
+	block := NewBlock(startLine, endLine)
+	err = cnx.Model(bs).Association("Blocks").Append(block)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cnx.Save(bs).Error; err != nil {
+		return nil, err
+	}
+
+	return block, nil
+}
+
+func UpdateBlock(sqlitedb string, block *Block, isSuccess bool, startLine, endLine int) (*Block, error) {
 	cnx, err := Connect(sqlitedb)
 	if err != nil {
 		logging.Log(logging.Error, err.Error())
@@ -70,24 +123,22 @@ func UpdateBatchStatistic(sqlitedb string, bs *BatchStatistic, isSuccess bool, f
 	}()
 
 	currentTime := time.Now()
-	bs.UpdatedAt = currentTime
+	block.UpdatedAt = currentTime
 	if isSuccess {
-		bs.SuccessCount = bs.SuccessCount + 1
+		block.SuccessCount = block.SuccessCount + 1
 	} else {
-		bs.FailureCount = bs.FailureCount + 1
-		cnx.Model(bs).Association("FailureRanges").Append(&FailureRange{
-			ID:        uuid.GenerateUID(),
-			StartLine: failureStartLine,
-			EndLine:   failureEndLine,
-			CreatedAt: currentTime,
-		})
+		block.FailureCount = block.FailureCount + 1
+		failureRange := NewFailureRange(startLine, endLine)
+		if err := cnx.Model(block).Association("FailureRanges").Append(failureRange); err != nil {
+			return nil, err
+		}
 	}
 
-	if err := cnx.Save(bs).Error; err != nil {
+	if err := cnx.Save(block).Error; err != nil {
 		return nil, err
 	}
 
-	return bs, nil
+	return block, nil
 }
 
 func GetBatchStatistics(sqlitedb string, pageNum int, pageSize int) ([]BatchStatistic, error) {
@@ -107,7 +158,7 @@ func GetBatchStatistics(sqlitedb string, pageNum int, pageSize int) ([]BatchStat
 		dbCnx.Close()
 	}()
 
-	err = cnx.Model(&BatchStatistic{}).Preload("FailureRanges").Find(&batchStatistics).Offset(pageNum).Limit(pageSize).Error
+	err = cnx.Model(&BatchStatistic{}).Preload("Blocks").Find(&batchStatistics).Offset(pageNum).Limit(pageSize).Error
 	if err != nil {
 		logging.Log(logging.Error, err.Error())
 		return nil, err
