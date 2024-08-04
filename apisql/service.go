b4a6768254d42ae3b5519258d5d42eb8b68128f1
willbrid <ngaswilly77@gmail.com>
2024-08-04 18:17:20 +0100

    refactor: update timeout injection and implement executeBatchSQLQuery func

--- apisql/service.go
+++ apisql/service.go
@@ -5,8 +5,12 @@ import (
 	"api-gateway-sql/db"
 	"api-gateway-sql/db/stat"
 	"api-gateway-sql/logging"
+	"api-gateway-sql/utils/file"
+	"mime/multipart"
 
 	"fmt"
+	"strings"
+	"sync"
 
 	"gorm.io/gorm"
 )
@@ -27,7 +31,7 @@ func getTargetAndDatabase(config config.Config, targetName string) (*config.Targ
 }
 
 // executeSingleSQLQuery used to execute a single sql query from target
-func executeSingleSQLQuery(target config.Target, database config.Database, timeout int, postParams map[string]interface{}) (db.SelectResult, error) {
+func executeSingleSQLQuery(target config.Target, database config.Database, postParams map[string]interface{}) (db.SelectResult, error) {
 	var (
 		cnx    *gorm.DB
 		result db.SelectResult = nil
@@ -35,7 +39,7 @@ func executeSingleSQLQuery(target config.Target, database config.Database, timeo
 	)
 
 	dbInstance := db.NewDatabase(database.Type)
-	cnx, err = dbInstance.Connect(database, timeout)
+	cnx, err = dbInstance.Connect(database)
 	if err != nil {
 		logging.Log(logging.Error, err.Error())
 		return nil, err
@@ -51,15 +55,44 @@ func executeSingleSQLQuery(target config.Target, database config.Database, timeo
 }
 
 // executeBatchSQLQuery used to execute a batch query from target
-func executeBatchSQLQuery(target config.Target, database config.Database, timeout int, filePath string) error {
+func executeBatchSQLQuery(sqlitedb string, target config.Target, database config.Database, postFile multipart.File) error {
+	var (
+		buffers []file.Buffer
+		block   *stat.Block
+		bs      *stat.BatchStatistic = stat.NewBatchStatistic(target.Name)
+		err     error                = nil
+	)
+
+	buffers, err = file.ReadCSVInBuffer(postFile, target.BufferSize)
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		return err
+	}
+
+	err = stat.AddBatchStatistic(sqlitedb, bs)
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		return err
+	}
+
+	for _, buffer := range buffers {
+		block, err = stat.AddNewBlockToBatchStatistic(sqlitedb, bs, buffer.StartLine, buffer.EndLine)
+		if err == nil {
+			go processBatch(sqlitedb, block, buffer, target, database)
+		}
+	}
+
+	return nil
+}
+
+func processBatch(sqlitedb string, block *stat.Block, buffer file.Buffer, target config.Target, database config.Database) error {
 	var (
 		cnx *gorm.DB
-		// stat stat.BatchStatistic = stat.NewBatchStatistic(target.Name)
 		err error = nil
 	)
 
 	dbInstance := db.NewDatabase(database.Type)
-	cnx, err = dbInstance.Connect(database, timeout)
+	cnx, err = dbInstance.Connect(database)
 	if err != nil {
 		logging.Log(logging.Error, err.Error())
 		return err
@@ -68,6 +101,53 @@ func executeBatchSQLQuery(target config.Target, database config.Database, timeou
 		dbCnx, _ := cnx.DB()
 		dbCnx.Close()
 	}()
+
+	var (
+		wg      sync.WaitGroup
+		record  map[string]interface{}
+		records []map[string]interface{}
+	)
+	batchSize := target.BatchSize
+	batchFields := strings.Split(target.BatchFields, ";")
+	numBatches := (len(buffer.Lines) + batchSize - 1) / batchSize
+
+	for i := 0; i < numBatches; i++ {
+		start := i * batchSize
+		end := start + batchSize
+		if end > len(buffer.Lines) {
+			end = len(buffer.Lines)
+		}
+
+		batch := buffer.Lines[start:end]
+		wg.Add(1)
+
+		go func(batch [][]string) {
+			defer wg.Done()
+			records = make([]map[string]interface{}, len(batch))
+
+			for _, line := range batch {
+				record, err = mapBatchFieldToValueLine(batchFields, line)
+				if err != nil {
+					logging.Log(logging.Error, "%s : %s", err.Error(), line)
+					break
+				} else {
+					records = append(records, record)
+				}
+			}
+
+			if len(records) > 0 {
+				err = dbInstance.ExecuteBatch(target.SqlQuery, records)
+				if err != nil {
+					logging.Log(logging.Error, err.Error())
+					stat.UpdateBlock(sqlitedb, block, false, start, end)
+				} else {
+					stat.UpdateBlock(sqlitedb, block, true, start, end)
+				}
+			}
+		}(batch)
+	}
+
+	wg.Wait()
 
 	return nil
 }
@@ -103,14 +183,14 @@ func mapBatchFieldToValueLine(fields []string, values []string) (map[string]inte
 }
 
 // executeInitSQLQuery used to populate a target database for testing
-func executeInitSQLQuery(sqlQuery string, database config.Database, timeout int) error {
+func executeInitSQLQuery(sqlQuery string, database config.Database) error {
 	var (
 		cnx *gorm.DB
 		err error = nil
 	)
 
 	dbInstance := db.NewDatabase(database.Type)
-	cnx, err = dbInstance.Connect(database, timeout)
+	cnx, err = dbInstance.Connect(database)
 	if err != nil {
 		logging.Log(logging.Error, err.Error())
 		return err
