029d06cb8e771bae1a00d9e28c6e37cf26678e82
willbrid <ngaswilly77@gmail.com>
2024-08-04 10:24:32 +0100

    feat: define get stats api

--- apisql/handler.go
+++ apisql/handler.go
@@ -4,6 +4,7 @@ import (
 	"api-gateway-sql/config"
 	"api-gateway-sql/logging"
 	"api-gateway-sql/utils/httputil"
+	"strconv"
 
 	"encoding/json"
 	"fmt"
@@ -84,6 +85,84 @@ func ApiPostSqlHandler(resp http.ResponseWriter, req *http.Request, config confi
 	}
 
 	httputil.SendJSONResponse(resp, http.StatusOK, httputil.HTTPStatusOKMessage, response)
+}
+
+// ApiPostSqlBatchHandler godoc
+// @Summary      Execute batch sql query
+// @Description  Execute batch sql query with values from a csv file
+// @Tags         apisql
+// @Accept       json
+// @Produce      json
+// @Param        target path  string  true  "Target Name"
+// @Param        csvfile  formData  file  true  "CSV Data to import"
+// @Success      200  {object}  httputil.HTTPResp
+// @Failure      400  {object}  httputil.HTTPResp
+// @Failure      500  {object}  httputil.HTTPResp
+// @Security     BasicAuth
+// @Router       /api-gateway-sql/{target}/batch [post]
+func ApiPostSqlBatchHandler(resp http.ResponseWriter, req *http.Request, config config.Config) {
+	vars := mux.Vars(req)
+	targetName := vars["target"]
+
+	_, _, err := getTargetAndDatabase(config, targetName)
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		httputil.SendJSONResponse(resp, http.StatusInternalServerError, err.Error(), nil)
+		return
+	}
+
+	_, _, err = req.FormFile("csvfile")
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		httputil.SendJSONResponse(resp, http.StatusBadRequest, "Unable to read the SQL file", nil)
+		return
+	}
+
+	httputil.SendJSONResponse(resp, http.StatusOK, httputil.HTTPStatusOKMessage, nil)
+}
+
+// ApiGetStatsHandler godoc
+// @Summary      Get statistics
+// @Description  Get all batch statistics
+// @Tags         apisql
+// @Accept       json
+// @Produce      json
+// @Param        page_num query  int  false  "Page number" default(1)
+// @Param        page_size query int  false  "Page size" default(20)
+// @Success      200  {object}  httputil.HTTPResp
+// @Failure      400  {object}  httputil.HTTPResp
+// @Failure      500  {object}  httputil.HTTPResp
+// @Security     BasicAuth
+// @Router       /api-gateway-sql/stats [get]
+func ApiGetStatsHandler(resp http.ResponseWriter, req *http.Request, config config.Config) {
+	queries := req.URL.Query()
+	pageNum, err := strconv.Atoi(queries.Get("page_num"))
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		httputil.SendJSONResponse(resp, http.StatusBadRequest, "Unable to handle page_num", nil)
+		return
+	}
+	pageSize, err := strconv.Atoi(queries.Get("page_size"))
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		httputil.SendJSONResponse(resp, http.StatusBadRequest, "Unable to handle page_size", nil)
+		return
+	}
+
+	stats, err := getStats(config.ApiGatewaySQL.Sqlitedb, pageNum, pageSize)
+	if err != nil {
+		httputil.SendJSONResponse(resp, http.StatusInternalServerError, httputil.HTTPStatusInternalServerErrorMessage, nil)
+		return
+	}
+
+	jsonStats, err := json.Marshal(stats)
+	if err != nil {
+		logging.Log(logging.Error, err.Error())
+		httputil.SendJSONResponse(resp, http.StatusInternalServerError, httputil.HTTPStatusInternalServerErrorMessage, nil)
+		return
+	}
+
+	httputil.SendJSONResponse(resp, http.StatusOK, httputil.HTTPStatusOKMessage, string(jsonStats))
 }
 
 // InitializeDatabaseHandler godoc
