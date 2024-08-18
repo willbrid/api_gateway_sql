e9fce75173dead445b1a5efd86e468d8ebc10104
willbrid <ngaswilly77@gmail.com>
2024-08-18 11:56:44 +0100

    fix: fixed postgres port dsn and timeout value

--- db/postgres.go
+++ db/postgres.go
@@ -19,7 +19,7 @@ func (postgresDB *PostgresDatabase) Connect(db config.Database) (*gorm.DB, error
 		sslMode = "enable"
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v sslmode=%v connect_timeout=%v", db.Host, db.Username, db.Password, db.Dbname, db.Host, sslMode, int(db.Timeout))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v sslmode=%v connect_timeout=%v", db.Host, db.Username, db.Password, db.Dbname, db.Port, sslMode, db.Timeout.Seconds())
 
 	cnx, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
